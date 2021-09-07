4be7081fcb172b10661e9bc0ccf978f541c99517
Xue Li <xue.lee@ed.ac.uk>
2021-09-07 23:18:56 +0100

    Update AAAI_evaluation.py

--- AAAI2021_Ent/AAAI_evaluation.py
+++ AAAI2021_Ent/AAAI_evaluation.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from datetime import datetime
 import subprocess
 import re
@@ -12,29 +6,22 @@ from os import listdir
 from os.path import isfile, join
 
 # set the working directory for prolog.
-working_directory = '/Users/lixue/GoogleDrive/01PHD/01program/eclipse-workspace/ABC_Clean/src/'
-# set the path of the faulty theories to repair.
-evaluationFiles = working_directory+'aaai'
-TheoryFiles = sorted([f for f in listdir(evaluationFiles) if isfile(join(evaluationFiles, f))])
-print(TheoryFiles)
-
-def main():
+working_directory = 'C:/Users/edmun/Downloads/xue/code/'
+# input the folder where the faulty theories to repair are.
+def main(cat):
     i = 1
     yLineH =[]
     yLineNoh =[]
     diff1 =[]
     diff2 = []
+    evaluationFiles = working_directory+cat
+    TheoryFiles = sorted([f for f in listdir(evaluationFiles) if isfile(join(evaluationFiles, f))])
+    print(TheoryFiles)
     while i < len(TheoryFiles):
         Theory = TheoryFiles[i]
         print(Theory)
     
-        proc =subprocess.run(["swipl", "-l", "aaai/"+Theory, "-g", "abc", "-g", "halt"], 
+        proc =subprocess.run(["swipl", "-l", cat+Theory, "-g", "abc", "-g", "halt"], 
                              timeout=None, cwd = working_directory, stdout=subprocess.PIPE)
         i+=1
     print('finished')
-
-
-
-
-
-
