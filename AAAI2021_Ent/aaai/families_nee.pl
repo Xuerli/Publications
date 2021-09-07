2e494e4db081e3536fa58d228a6abb6cbf5edc5c
Xue Li <xue.lee@ed.ac.uk>
2021-09-07 22:30:12 +0100

    Add files via upload

---
+++ AAAI2021_Ent/aaai/families_nee.pl
@@ -0,0 +1,13 @@
+:- working_directory(_, '/Users/lixue/GoogleDrive/01PHD/01program/eclipse-workspace/ABC_Clean/src/').
+:-[main].
+
+axiom([+parent(a,b,birth)]).
+axiom([+parent(a,c,step)]).
+axiom([+families(\x,\y), -parent(\x,\y,birth)]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Preferred Structure:
+
+trueSet([families(a,b),families(a,c)]).
+falseSet([]).
+protect([]).
+heuristics([noEE]).
+theoryFile:- !.
