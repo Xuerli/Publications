cb7ce36bfa6adb5b8bd4b4e332ec9db5be7ee9fd
Xue Li <xue.lee@ed.ac.uk>
2021-09-07 23:12:56 +0100

    Add files via upload

---
+++ AAAI2021_Ent/ee/tweetynh.pl
@@ -0,0 +1,19 @@
+:- working_directory(_, '/Users/lixue/GoogleDrive/01PHD/01program/eclipse-workspace/ABC_Clean/src/').
+:-[main].
+
+
+axiom([+penguin(tweety)]).
+axiom([+bird(polly)]).
+axiom([-bird(\x), +fly(\x)]).
+axiom([-penguin(\y), +bird(\y)]).
+axiom([-bird(\y), +feather(\y)]).
+
+
+trueSet([feather(tweety),feather(tweety), fly(bird)]).
+falseSet([fly(tweety)]).
+
+
+
+protect([]).
+heuristics([]).
+theoryFile:- !.
